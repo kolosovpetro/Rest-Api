814e42ed26990e88901188d8716c3c3f67a3d12f
razumovskiy <kolosovp94@gmail.com>
2020-09-08 20:59:19 +0300

    dto files added

---
+++ Api.Models/DTO/MovieUpdateDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api.Models.DTO
+{
+    public class MovieUpdateDto
+    {
+        [Required]
+        [MaxLength(250)]
+        public string Title { get; set; }
+        
+        [Required]
+        public int Year { get; set; }
+        
+        [Required]
+        public int AgeRestriction { get; set; }
+        
+        [Required]
+        public float Price { get; set; }
+    }
+}
---
+++ Api.Models/DTO/MoviesReadDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api.Models.DTO
+{
+    public class MoviesReadDto
+    {
+        [Required]
+        public int MovieId { get; set; }
+        
+        [Required]
+        [MaxLength(250)]
+        public string Title { get; set; }
+        
+        [Required]
+        public int Year { get; set; }
+        
+        [Required]
+        public int AgeRestriction { get; set; }
+        
+        [Required]
+        public float Price { get; set; }
+    }
+}
