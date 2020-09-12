01409832f370a77667e1da43cced75a676b9269b
petro kolosov <kolosovp94@gmail.com>
2020-09-12 15:28:03 +0300

    megrged

---
+++ Api.Repositories/Extensions/RepositoriesDI.cs
@@ -0,0 +1,14 @@
+﻿using Api.Repositories.Base;
+using Api.Repositories.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Api.Repositories.Extensions
+{
+    public static class RepositoriesDI
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            return services.AddScoped(typeof(IRepository<>), typeof(RepositoryBase<>));
+        }
+    }
+}
