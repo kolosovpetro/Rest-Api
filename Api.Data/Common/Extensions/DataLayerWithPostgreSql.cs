4febcea1a006bef0c19cfd14b8de5985b6227a9c
razumovskiy <kolosovp94@gmail.com>
2020-10-22 13:16:35 +0300

    project updated

--- Api.Data/Common/Extensions/DataLayerWithPostgreSql.cs
+++ Api.Data/Common/Extensions/DataLayerWithPostgreSql.cs
@@ -11,12 +11,12 @@ namespace Api.Data.Common.Extensions
         public static IServiceCollection AddDataLayerWithPostgreSql(this IServiceCollection services,
             IConfiguration configuration)
         {
-            var environmentConnectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
+            var environmentConnectionString = Environment.GetEnvironmentVariable("HEROKU_POSTGRE_CONNECTION_STRING");
 
             services.AddDbContext<PostgreContext>(options =>
                 options.UseNpgsql(
                     environmentConnectionString ??
-                    configuration.GetConnectionString("HEROKU_POSTGRE_CONNECTION_STRING")));
+                    configuration.GetConnectionString("LOCAL_POSTGRES_CONNECTION_STRING")));
 
             services.AddTransient<DbContext, PostgreContext>();
             return services;
