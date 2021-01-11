af18361dfd5d642f910580f1a97505b97a8548c2
Petro <kolosovp94@gmail.com>
2021-01-11 19:15:58 +0100

    controller test added

---
+++ Api.Tests/Controller/ControllerGetAllMoviesTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using Api.Controllers;
+using Api.Models.Models;
+using Api.Repositories.Interfaces;
+using Api.Services.Services;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+
+namespace Api.Tests.Controller
+{
+    [TestFixture]
+    public class ControllerGetAllMoviesTest
+    {
+        public async Task Controller_GetAll_Test()
+        {
+            // Arrange
+            var mockRepo = new Mock<IRepository<Movies>>();
+            mockRepo.Setup(getAll => getAll.GetAll())
+                .Returns(TestHelper.AllMovies);
+
+            var mockServices = new Mock<MoviesService>(mockRepo.Object, TestHelper.Mapper);
+            mockServices.Setup(getAllMoviesAsync => getAllMoviesAsync.GetAllMoviesAsync())
+                .Returns(TestHelper.MappedMovies(TestHelper.AllMovies, TestHelper.Mapper));
+            var mockController = new Mock<MoviesController>(mockServices.Object);
+            
+            // Act
+            var result = await mockController.Object.GetAll();
+
+            // Assert
+            result.GetType().Should().Be(typeof(IActionResult));
+            var okResult = result as OkObjectResult;
+            okResult.Should().NotBeNull();
+            var objResult = result as ObjectResult;
+            objResult.Should().NotBeNull();
+            objResult?.StatusCode.Should().Be(200);
+        }
+    }
+}
---
+++ Api.Tests/Controller/TestHelper.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Api.MapperFiles;
+using Api.Models.DTO;
+using Api.Models.Models;
+using AutoMapper;
+
+namespace Api.Tests.Controller
+{
+    public static class TestHelper
+    {
+        public static Task<IEnumerable<MoviesReadDto>> MappedMovies(IEnumerable<Movies> collection, IMapperBase mapper)
+        {
+            return Task.FromResult(mapper.Map<IEnumerable<MoviesReadDto>>(collection));
+        }
+
+        public static readonly IEnumerable<Movies> AllMovies = new List<Movies>
+        {
+            new Movies(1, "Star Wars Episode IV: A New Hope", 1979, 12, 10f),
+            new Movies(2, "Ghostbusters", 1984, 12, 5.5f),
+            new Movies(3, "Terminator", 1984, 15, 8.5f),
+            new Movies(4, "Taxi Driver", 1976, 17, 5f),
+            new Movies(5, "Platoon", 1986, 18, 5f),
+            new Movies(6, "Frantic", 1988, 15, 8.5f),
+            new Movies(7, "Ronin", 1998, 13, 9.5f),
+            new Movies(8, "Analyze This", 1999, 16, 10.5f),
+            new Movies(9, "Leon: the Professional", 1994, 16, 8.5f),
+            new Movies(10, "Mission Impossible", 1996, 13, 8.5f)
+        };
+
+        private static readonly MoviesProfile Profile = new MoviesProfile();
+
+        private static readonly MapperConfiguration Configuration = 
+            new MapperConfiguration(cfg => cfg.AddProfile(Profile));
+
+        public static readonly Mapper Mapper = new Mapper(Configuration);
+    }
+}
